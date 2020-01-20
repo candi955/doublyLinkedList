5d4a7963bde112b21d60ed639b548f1b00b78999
candi955 <candace.m.norris@gmail.com>
2020-01-19 23:06:25 -0700

    Update

--- Menu.py
+++ Menu.py
@@ -1,22 +1,233 @@
-from DoublyLinkedListPage import Node, DoublyLinkedList
 
-nodey = Node(data=None)
-doubList = DoublyLinkedList()
+# Reference: https://stackabuse.com/doubly-linked-list-with-python-examples/
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.nref = None
+        self.pref = None
+Node(data=None)
 
-def mainMenu():
+class DoublyLinkedList:
+    def __init__(self):
+        self.start_node = None
+
+    # inserting data into an empty list (Insert at end of list)
+    def insert_in_emptylist(self, data):
+            if self.start_node is None:
+                new_node = Node(data)
+                self.start_node = new_node
+            else:
+                print("list is not empty")
+
+
+    def insert_at_start(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            print("node inserted")
+            return
+        new_node = Node(data)
+        new_node.nref = self.start_node
+        self.start_node.pref = new_node
+        self.start_node = new_node
+
+    def insert_at_end(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        new_node = Node(data)
+        n.nref = new_node
+        new_node.pref = n
+
+
+    def insert_after_item(self, x, data):
+        if self.start_node is None:
+            print("List is empty")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                if n.item == x:
+                    break
+                n = n.nref
+            if n is None:
+                print("item not in the list")
+            else:
+                new_node = Node(data)
+                new_node.pref = n
+                new_node.nref = n.nref
+                if n.nref is not None:
+                    n.nref.prev = new_node
+                n.nref = new_node
+
+    def insert_before_item(self, x, data):
+        if self.start_node is None:
+            print("List is empty")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                if n.item == x:
+                    break
+                n = n.nref
+            if n is None:
+                print("item not in the list")
+            else:
+                new_node = Node(data)
+                new_node.nref = n
+                new_node.pref = n.pref
+                if n.pref is not None:
+                    n.pref.nref = new_node
+                n.pref = new_node
+
+
+    def traverse_list(self):
+        if self.start_node is None:
+            print("List has no element")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item, " ")
+                n = n.nref
+
+    def delete_at_start(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        self.start_node = self.start_node.nref
+        self.start_prev = None;
+
+     # deleting elements at the end of the list
+    def delete_at_end(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            self.start_node = None
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        n.pref.nref = None
+
+    # deleting elements by value
+    def delete_element_by_value(self, x):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            if self.start_node.item == x:
+                self.start_node = None
+            else:
+                print("Item not found")
+            return
+
+        if self.start_node.item == x:
+            self.start_node = self.start_node.nref
+            self.start_node.pref = None
+            return
+
+        n = self.start_node
+        while n.nref is not None:
+            if n.item == x:
+                break;
+            n = n.nref
+        if n.nref is not None:
+            n.pref.nref = n.nref
+            n.nref.pref = n.pref
+        else:
+            if n.item == x:
+                n.pref.nref = None
+            else:
+                print("Element not found")
+
+    # Reversing a doubly linked list
+    def reverse_linked_list(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        p = self.start_node
+        q = p.nref
+        p.nref = None
+        p.pref = q
+        while q is not None:
+            q.pref = q.nref
+            q.nref = p
+            p = q
+            q = q.pref
+        self.start_node = p
+
+    # Reference: https://stackabuse.com/doubly-linked-list-with-python-examples/
+
+    # Attempting to create a function for 'find'
+    def find_element_by_value(self, x):
+
+        # for checking first node and finding the beginning number
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        if self.start_node.nref is None:
+            if self.start_node.item == x:
+                print('Item found.')
+
+            else:
+                print("Item not found")
+            return
+
+        if self.start_node.item == x:
+            print("The number", x, "is the first number located on the list.")
+            self.start_node = self.start_node
+            self.start_node.pref = None
+            return
+        # for finding the middle numbers
+        n = self.start_node
+        while n.nref is not None:
+            if n.item == x:
+                print("The number", x, "is one of the numbers located in the middle of the list.")
+                break
+            n = n.nref
+
+        # for finding the end number
+        if n.nref is not None:
+            #n.pref.nref = n.nref
+            n.nref.pref = n.pref
+        else:
+            if n.item == x:
+                print("The number", x, "is the last number on the list.")
+            else:
+                print("Element not found")
+
+
+DoublyLinkedList()
+
+# Reference: https://stackabuse.com/doubly-linked-list-with-python-examples/
+
+class mainMenu():
+
+    newList = DoublyLinkedList()
 
     print("Program choice key:\n" +
           "1 - for Enter number in empty list\n" +
           "2 - for Insert number at start of list\n" +
-          "3 - Insert number after another number of your choice" +
-          "4 - ")
-    userChoice = input('Please enter your command to utilize the DoublyLinkedList: ')
-    print(userChoice)
+          "3 - Insert number at end of the list" +
+          "4 - Insert number after another number of your choice" +
+          "5 - ")
 
-    userNumber = input('Please enter what number you would like to work with: ' )
+    print('\n')
+
+    userChoice = input('Please enter your command to utilize the DoublyLinkedList: ')
 
     if userChoice == 1:
-        doubList(userNumber)
+        print('hello')
+
 
 
 mainMenu()
